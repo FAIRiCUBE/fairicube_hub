71682aa5c6e8a29e1f8794ccb45db0f745ebce2c
jetschny <jetschny@users.noreply.github.com>
2025-10-10 08:23:51 +0000

    deploy: 9046cf7875546ec698907fafecff016d24abf9a5

---
+++ bin/deploy_to_hub.fairicube.eu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# To be executed on the hub.fairicube.eu server.
+
+www_dir="/home/fairicube_hub/public_html"
+git_dir="/home/fairicube_hub/fairicube_hub"
+
+pushd "$git_dir" > /dev/null || exit 1
+
+if ! git pull 2>&1 | grep -q 'Already up to date'; then
+  echo -e "\n\n$(date) building and deploying website...\n\n"
+  bundle exec jekyll build || { echo "building website failed"; exit 1; }
+  rsync -az --delete _site/ "$www_dir/" || { echo "syncing website failed"; exit 1; }
+  echo "done."
+else
+  echo "already up to date"
+fi
+
+popd > /dev/null || exit 1
